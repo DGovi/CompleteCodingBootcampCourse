f6290db79ad256f737afab138fcd8a6aa13aa818
Daniel Gauvin <danielgauvin01@gmail.com>
2020-05-06 22:38:38 -0400

    renamed file for formatting

---
+++ CompletePythonBootcampHMWK1q1.py
@@ -0,0 +1,10 @@
+# *******************First question********************************** 
+# write a function that computes the volume of a sphere given its radius
+
+import math
+
+def vol(rad):
+	return math.pi * rad ** 2; 
+
+#test
+#print(vol(2));
