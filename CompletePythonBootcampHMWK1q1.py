db66426af30e8efb528da25647d1be193e89831c
Daniel Gauvin <danielgauvin01@gmail.com>
2020-05-06 22:57:45 -0400

    renamed again and completed two ?? questions

--- CompletePythonBootcampHMWK1q1.py
+++ CompletePythonBootcampHMWK1q1.py
@@ -8,3 +8,30 @@ def vol(rad):
 
 #test
 #print(vol(2));
+
+#*******************************Second Question ***************************
+# write a function that checks whether a number is in a givin range ( inclusive of high and low)
+
+def inRange(num, high, low):
+	return low <=  num <= high;
+
+#test
+#print(inRange(1,2,3))
+
+#******************************Third  Question ***************************
+#write a function thhat accepts a string and calculates the number of upper case and lower case
+
+def numUpperAndLower(aString):
+	numUpper = 0;
+	numLower = 0;
+
+	for letter in aString:
+		if letter.isupper():
+			numUpper += 1;
+		else:
+			numLower += 1;
+	print("there are " + str(numUpper) + " upper case and " + str(numLower) + " lower case")
+
+#test
+#numUpperAndLower("DAniel")
+
