cd30cfe4956b4288359cfe09ec111491f77869ad
Daniel Gauvin <danielgauvin01@gmail.com>
2020-06-09 16:38:53 -0400

    refactoredthisturn() method

--- TICTACETOE - Milestone1.py
+++ TICTACETOE - Milestone1.py
@@ -20,17 +20,35 @@ gameIsFinished = False
 turn = 0
 playAgain = False
 
-print("Lets play TICTACTOE \n")
-print("player 1 is x")
-print("player 2 is o \n")
+
+def chose_game_mode():
+    print("Lets play TICTACTOE \n")
+    game_mode = int(input(
+        "which game mode are you playing? \n 1-Player vs Player? \n 2-Player vs Computer?\n (enter 1 or 2)"))
+    if game_mode == 1:
+        print("PVP CHOSEN")
+        print("player 1 is x")
+        print("player 2 is o \n")
+        pvp()
+    elif game_mode == 2:
+        pass
+##########################################################
 
 
-def newGame():
-    global currentBoard, gameIsFinished, turn, playAgain
-    currentBoard = ["0", " ", " ", " ", " ", " ", " ", " ", " ", " "]
-    gameIsFinished = False
-    turn = 0
-    playAgain = False
+def pvp():  # player vs player
+    displayBoard(currentBoard)
+    playGame()
+
+    while playAgain:
+        playGame()
+    print("Exiting TICTACTOE........")
+
+
+'''
+def pvc():
+    print("VS COMPUTER CHOSEN")
+    print("you go first")
+'''
 
 
 def displayBoard(board):
@@ -44,6 +62,11 @@ def displayBoard(board):
 
 def playGame():
     global gameIsFinished, turn, playAgain, currentBoard
+    currentBoard = ["0", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    gameIsFinished = False
+    turn = 0
+    playAgain = False
+
     while not gameIsFinished:
         thisTurn(currentBoard)
         gameIsFinished = isWinningMove(currentBoard) or turn == 8
@@ -59,31 +82,29 @@ def playGame():
 
 
 def thisTurn(board):
+    if turn % 2 == 0:
+        print("Player 1 turn!")
+        player_input = ensure_valid_position(board)
+        currentBoard[player_input] = 'x'
+    else:
+        print("Player 2 turn!")
+        player_input = ensure_valid_position(board)
+        currentBoard[player_input] = 'o'
+
+
+def ensure_valid_position(board):
     while True:
         try:
-            if turn % 2 == 0:
-                print("Player 1 turn!")
-                player1Input = int(
-                    input("where would you like to place your x (enter int between 1 and 9) \n"))
-                if (player1Input < 1 or player1Input > 9):
-                    raise Exception
-                elif (board[player1Input] == 'x' or board[player1Input] == 'o'):
-                    raise Exception
-                else:
-                    currentBoard[player1Input] = 'x'
+            playerInput = int(
+                input("Which position (enter int between 1 and 9) \n"))
+            if (playerInput < 1 or playerInput > 9):
+                raise Exception
+            elif (board[playerInput] == 'x' or board[playerInput] == 'o'):
+                raise Exception
             else:
-                print("Player 2 turn!")
-                player2Input = int(
-                    input("where would you like to place your o (enter int between 1 and 9) \n"))
-                if (player2Input < 1 or player2Input > 9):
-                    raise Exception
-                elif (board[player2Input] == 'x' or board[player2Input] == 'o'):
-                    raise Exception
-                else:
-                    currentBoard[player2Input] = 'o'
-            break
+                return playerInput
         except:
-            print("Enter a VALID number")
+            print("Enter a vaild number")
             continue
 
 
@@ -100,24 +121,10 @@ def isWinningMove(board):
         # check if positinos are board spaces
         if(board[1] == " " or board[2] == " " or board[3] == " " or board[4] == " " or board[7] == " " or board[9] == " "):
             return False
-        print("youve won!!")
+        print("winner!!")
         return True
     else:
         return False
 
 
-def isMoveAllowed(position, board):
-    if (position < 1 or position > 9 or board[position] == 'x' or board[position] == 'o'):
-        return False
-    return True
-
-
-# game start
-newGame()
-displayBoard(currentBoard)
-playGame()
-
-while playAgain:
-    newGame()
-    playGame()
-print("Exiting TICTACTOE........")
+pvp()
